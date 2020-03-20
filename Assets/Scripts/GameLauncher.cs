36cd60f2de2e622bab4f1ccf20488e2e8553d261
kprasad1221 <kprasad.1221@gmail.com>
2020-03-19 21:02:03 -0800

    add music and level manager

---
+++ Assets/Scripts/GameLauncher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLauncher : MonoBehaviour
+{
+
+    [Range(1.5f, 10f)] [SerializeField] float levelLoadDelay = 3f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadLevel", levelLoadDelay);
+    }
+
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene(1);  
+    }
+}
