b72c7f819217ba23a1e90c3b6e46186bc75d50bd
kprasad1221 <kprasad.1221@gmail.com>
2020-03-21 13:49:03 -0800

    added fx and yaw pitch roll

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -10,6 +11,14 @@ public class Player : MonoBehaviour
     [Tooltip("In m")][SerializeField] float xRange = 3.5f;
     [Tooltip("In m")] [SerializeField] float yRangeMin = -1f;
     [Tooltip("In m")] [SerializeField] float yRangeMax = 4f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +28,34 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffsetThisFrame = xSpeed * horizontalThrow * Time.deltaTime;
-        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        float yOffsetThisFrame = ySpeed * verticalThrow * Time.deltaTime;
-      //  Debug.Log("Horizontal Throw:" + horizontalThrow);
-      //  Debug.Log("xOffset:" + xOffsetThisFrame);
-        
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffsetThisFrame = xSpeed * xThrow * Time.deltaTime;
+        float yOffsetThisFrame = ySpeed * yThrow * Time.deltaTime;
+
         float rawXOffset = transform.localPosition.x + xOffsetThisFrame;
         float clampXOffset = Mathf.Clamp(rawXOffset, -xRange, xRange);
         float rawYOffset = transform.localPosition.y + yOffsetThisFrame;
         float clampYOffset = Mathf.Clamp(rawYOffset, yRangeMin, yRangeMax);
+
         transform.localPosition = new Vector3(clampXOffset, clampYOffset, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float positionDueToControl = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + positionDueToControl;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
