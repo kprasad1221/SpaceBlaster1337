a530ab9949857c09bfdcbfe6136a658e69b275a3
kprasad1221 <kprasad.1221@gmail.com>
2020-04-06 15:38:41 -0800

    derp

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.AddComponent<BoxCollider>();
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        print("particle collided w/Enemy " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
