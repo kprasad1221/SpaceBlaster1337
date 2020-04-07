bffcaca65c83e59375cc2c50552005e86baa4b3b
kprasad1221 <kprasad.1221@gmail.com>
2020-04-06 16:13:18 -0800

    staged font

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Tooltip("Explosion Prefab with ParticleSys and AudioSrc")] [SerializeField] GameObject explosionFX;
+    [SerializeField] Transform parent;
+
     private void Start()
     {
-        gameObject.AddComponent<BoxCollider>();
+        AddNonTriggerBoxCollider();
     }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         print("particle collided w/Enemy " + gameObject.name);
+        GameObject fx = Instantiate(explosionFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        fx.name = gameObject.name + "_Explosion";
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    [Tooltip("In Seconds")] [SerializeField] float selfDestructDelayTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, selfDestructDelayTime);
+    }
+
+}
